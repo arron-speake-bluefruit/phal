e32762f09f5f7a97b330edbfb2d65d93de8adb56
Callum David O'Brien <cdo@teknik.io>
2020-03-05 14:33:52 +0000

    add a couple of simple integration tests

---
+++ tests/phal_tests.rs
@@ -0,0 +1,83 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * Copyright (C) 2020 Callum David O'Brien
+ */
+
+#[macro_use]
+extern crate phal;
+extern crate rocket;
+
+use phal::{
+    limb::{Error, Limb, LimbTypes},
+    server,
+};
+use rocket::{http::Status, local::Client};
+use serde_json as json;
+
+use std::{collections::HashMap, sync::Mutex};
+
+struct MockLimb(String);
+
+impl Limb for MockLimb {
+    fn from_json(_config: &json::Value) -> Option<Self> {
+        Some(MockLimb(String::new()))
+    }
+
+    fn get(&mut self) -> Result<String, Error> {
+        Ok(self.0.clone())
+    }
+
+    fn set(&mut self, value: String) -> Result<(), Error> {
+        self.0 = value;
+        Ok(())
+    }
+}
+
+#[test]
+fn server_has_endpoints_for_limbs_in_config() {
+    let types = limb_types![("foo", MockLimb)];
+    let config = r#"
+        {
+            "bar": {
+                "type": "foo"
+            },
+            "baz": {
+                "type": "foo"
+            }
+        }
+    "#;
+    let client = server::rocket(types, config.to_string())
+        .and_then(|r| Client::new(r).ok())
+        .unwrap();
+
+    assert_eq!(client.get("/limb/bar").dispatch().status(), Status::Ok);
+    assert_eq!(client.post("/limb/bar").dispatch().status(), Status::Ok);
+    assert_eq!(client.get("/limb/baz").dispatch().status(), Status::Ok);
+    assert_eq!(client.post("/limb/baz").dispatch().status(), Status::Ok);
+}
+
+#[test]
+fn get_and_post_requests_call_get_and_set_on_a_limb() {
+    let types = limb_types![("foo", MockLimb)];
+    let config = r#"
+        {
+            "bar": {
+                "type": "foo"
+            }
+        }
+    "#;
+    let client = server::rocket(types, config.to_string())
+        .and_then(|r| Client::new(r).ok())
+        .unwrap();
+
+    client.post("/limb/bar").body("baz").dispatch().status();
+    assert_eq!(
+        client.get("/limb/bar").dispatch().body_string(),
+        Some("baz".to_string())
+    );
+    client.post("/limb/bar").body("quux").dispatch().status();
+    assert_eq!(
+        client.get("/limb/bar").dispatch().body_string(),
+        Some("quux".to_string())
+    );
+}
