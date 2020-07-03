78686ec37e117015b7c5e7f4f335983322dfb42d
Callum David O'Brien <cdo@wip.sh>
2020-07-03 10:19:18 +0100

    Add carnix to dev shell

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ with (import <nixpkgs> {});
 mkShell {
   name = "dev-shell";
   buildInputs = [
-    cargo python38 python38Packages.pytest
+    cargo carnix python38 python38Packages.pytest
     python38Packages.requests rustfmt
   ];
 }
